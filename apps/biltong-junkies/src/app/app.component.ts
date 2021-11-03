f2cf5f1a504cb5adeb99c6201938696b58d56799
lcervo-diligent <lcervo@diligent.com>
2021-11-03 11:42:10 +0000

    feat: first commit

---
+++ apps/biltong-junkies/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lupin3d-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'biltong-junkies';
+}
